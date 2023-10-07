f92b30198a95eacfc7dd8b45712b816525498cad
Hunter Trujillo <cryptoquick@pm.me>
2023-10-07 09:41:39 -0600

    Refactor to use q-compressor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use bitpacking::{BitPacker, BitPacker8x};
+use q_compress::{Compressor, CompressorConfig};
 
 pub fn split(input: &[u8]) -> (Vec<u32>, Vec<u32>) {
     let mut headers = vec![];
     let mut indices = vec![];
 
     let mut header = true;
-    for chunk in input.chunks(3) {
+    for chunk in input.chunks_exact(3) {
         if header {
             headers.push(u32::from_le_bytes([chunk[2], chunk[1], chunk[0], 0x00]));
             header = false;
@@ -21,50 +21,32 @@ pub fn split(input: &[u8]) -> (Vec<u32>, Vec<u32>) {
     (headers, indices)
 }
 
-pub fn compress(data: &[u32]) -> (Vec<u8>, Vec<u8>) {
-    let bitpacker = BitPacker8x::new();
-    let mut initial_value = 0u32;
-    let mut result = vec![];
-    let mut compressed = vec![0u8; 4 * BitPacker8x::BLOCK_LEN];
-    let mut compressed_lens = vec![];
-    let mut lengths: Vec<u8> = vec![];
+pub fn join(headers: &[u32], indices: &[u32]) -> Vec<u8> {
+    let mut bytes = vec![];
 
-    for chunk in data.chunks_exact(BitPacker8x::BLOCK_LEN) {
-        let num_bits: u8 = bitpacker.num_bits(chunk);
-
-        // println!("num bits: {num_bits}");
-
-        let compressed_len =
-            bitpacker.compress_sorted(initial_value, chunk, &mut compressed[..], num_bits);
-
-        // println!("compressed len: {compressed_len}");
-
-        result.extend_from_slice(&compressed[..compressed_len]);
-
-        compressed_lens.push(compressed_len as u32);
-        initial_value = chunk[BitPacker8x::BLOCK_LEN - 1];
+    for (i, header) in headers.iter().enumerate() {
+        let header_bytes = header.to_le_bytes();
+        bytes.push(header_bytes[2]);
+        bytes.push(header_bytes[1]);
+        bytes.push(header_bytes[0]);
+        let index_bytes = indices[i].to_le_bytes();
+        bytes.push(index_bytes[2]);
+        bytes.push(index_bytes[1]);
+        bytes.push(index_bytes[0]);
     }
-    // TODO: handle remainder
 
-    let mut initial_value = 0u32;
+    bytes
+}
 
-    for chunk in compressed_lens.chunks_exact(BitPacker8x::BLOCK_LEN) {
-        let num_bits: u8 = bitpacker.num_bits(chunk);
+pub fn compress(data: &[u32]) -> Vec<u8> {
+    let mut compressor = Compressor::<u32>::from_config(
+        CompressorConfig::default()
+            .with_compression_level(8)
+            .with_delta_encoding_order(1)
+            .with_use_gcds(false),
+    );
 
-        // println!("num bits: {num_bits}");
-
-        let compressed_len =
-            bitpacker.compress_sorted(initial_value, chunk, &mut compressed[..], num_bits);
-
-        // println!("compressed len: {compressed_len}");
-
-        lengths.extend_from_slice(&compressed[..compressed_len]);
-
-        initial_value = chunk[BitPacker8x::BLOCK_LEN - 1];
-    }
-    // TODO: handle remainder
-
-    (result, lengths)
+    compressor.simple_compress(data)
 }
 
 #[cfg(test)]
@@ -72,50 +54,34 @@ mod tests {
     use std::fs;
 
     use anyhow::Result;
+    use q_compress::auto_decompress;
 
     use super::*;
 
     #[test]
-    fn bitpacker() -> Result<()> {
+    fn q_compressor() -> Result<()> {
         let file = fs::read("sealList.bin")?;
         println!("orig bytes: {}", file.len());
 
         let (headers, indices) = split(&file);
 
-        let (compressed_headers, hlens) = compress(&headers);
-        let (compressed_indices, ilens) = compress(&indices);
+        let compressed_headers = compress(&headers);
+        let compressed_indices = compress(&indices);
 
-        println!("compressed headers len: {}", compressed_headers.len());
-        println!("compressed indices len: {}", compressed_indices.len());
-
-        println!("compressed headers len: {}", hlens.len());
-        println!("compressed indices len: {}", ilens.len());
-
-        assert_eq!(
-            compressed_headers.len() + compressed_indices.len() + hlens.len() + ilens.len(),
-            file.len()
+        println!("compressed headers: {}", compressed_headers.len());
+        println!("compressed indices: {}", compressed_indices.len());
+        println!(
+            "total: {}",
+            compressed_headers.len() + compressed_indices.len()
         );
 
-        // assert_eq!(
-        //     (num_bits as usize) * BitPacker4x::BLOCK_LEN / 8,
-        //     compressed_len
-        // );
+        let decompressed_headers = auto_decompress::<u32>(&compressed_headers)?;
+        let decompressed_indices = auto_decompress::<u32>(&compressed_indices)?;
 
-        // // Decompressing
-        // let mut decompressed = vec![0u32; BitPacker4x::BLOCK_LEN];
+        let bytes = join(&decompressed_headers, &decompressed_indices);
 
-        // // The initial value must be the same as the one passed
-        // // when compressing the block.
-        // bitpacker.decompress_sorted(
-        //     initial_value,
-        //     &compressed[..compressed_len],
-        //     &mut decompressed[..],
-        //     num_bits,
-        // );
-
-        // assert_eq!(&data, &decompressed);
-
-        // assert_eq!(compressed.len(), 0);
+        assert_eq!(file[0..10], bytes[0..10]);
+        assert_eq!(file.len(), bytes.len());
 
         Ok(())
     }
